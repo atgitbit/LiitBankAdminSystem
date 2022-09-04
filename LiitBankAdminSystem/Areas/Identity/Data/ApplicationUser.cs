7d38073d2575b1caaaacf26133ec6e96586d163f
Linn Johansson <linn.Jswan@outlook.com>
2022-09-04 13:57:51 +0200

    Add project files.

---
+++ LiitBankAdminSystem/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace LiitBankAdminSystem.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+    //identityuser application object, authinasierar hela applikationen.
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+}
+

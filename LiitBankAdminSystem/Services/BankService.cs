49adaa8bd1f99fa5fac24589c1bcb6d1fc3a6ee1
Linn Johansson <linn.Jswan@outlook.com>
2022-09-11 23:36:51 +0200

    cashiercontroller removed

---
+++ LiitBankAdminSystem/Services/BankService.cs
@@ -0,0 +1,42 @@
+﻿using LiitBankAdminSystem.Areas.Identity.Data;
+using LiitBankAdminSystem.Models;
+
+namespace LiitBankAdminSystem.Services
+{
+    public class BankService : IBankService
+    {
+        private readonly ApplicationDbContext _context;
+        public BankService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Account FindAccountById(int id)
+        {
+            return _context.Accounts.FirstOrDefault(a => a.AccountId == id);
+        }
+        public void Withdraw(int id, decimal amount)
+        {
+            var fromAccount = _context.Accounts.FirstOrDefault(a => a.AccountId == id);
+            var balance = fromAccount.Balance;
+        }
+        public void Transaction(int id, int toAccId, decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+        public bool CheckWithdrawPossible(int id, decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateTransaction(Transaction transaction)
+        {
+            throw new NotImplementedException();
+        }
+
+    
+    
+
+     
+    }
+}
